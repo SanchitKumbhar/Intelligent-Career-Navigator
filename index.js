8f14f7a0943a8a116b5f748b2472af10ee60d2bc
Sanchit <sanchitskumbhar@gmail.com>
2025-07-29 20:31:45 +0530

    db connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,10 +7,26 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
+
+// filepath: /home/sanchit/Desktop/Intelligent Career Navigator /index.js
+const mongoose = require('mongoose');
+// ...existing code...
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// ...existing code...
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to Intelligent Career Navigator!');
 });
+
+
 
 // Start server
 app.listen(PORT, () => {
