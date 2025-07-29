4e64ae9a1c432b6073993cd998854a1ad4314ffd
Aniket_The_TechWhiz <creatorpanda26@gmail.com>
2025-07-29 23:04:22 +0530

    Update DB connection and package dependencies

--- dbconfig/db.js
+++ dbconfig/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useCreateIndex: true, // Deprecated in newer Mongoose versions
